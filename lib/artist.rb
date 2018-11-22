be3e1172a1bc905291cbf5de1e1b4497b7441eb6
Vincent Chan <vchan313@gmail.com>
2018-11-22 14:46:32 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #require 'pry'
 
 class Artist
@@ -12,3 +13,12 @@ end
 #artist = Artist.new("Beyonce")
 
 #binding.pry
+=======
+class Artist
+attr_accessor :name
+def initialize(name)
+	@name = name
+end
+
+end
+>>>>>>> 56b6cacf3e794f32aafda6e11c2cef8e330bfc5a
